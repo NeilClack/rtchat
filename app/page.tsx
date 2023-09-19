337d2235601e284c9dc1c9d84b4960cd157564d5
NeilClack <131825138+NeilClack@users.noreply.github.com>
2023-09-19 15:23:48 -0500

    Setup Redis database connection

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import Image from 'next/image'
 import Button from './components/UI/button'
+import { db } from './lib/db'
 
 export default function Home() {
+
   return (
     <main>
       <Button>Test Button</Button>
