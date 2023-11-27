58f10e9a4000177050e1749657e1696a693adee7
NeilClack <131825138+NeilClack@users.noreply.github.com>
2023-11-26 22:12:30 -0600

    Apply formatting

--- app/(dashboard)/dashboard/add/page.tsx
+++ app/(dashboard)/dashboard/add/page.tsx
@@ -2,11 +2,13 @@ import { FC } from "react";
 import AddFriendButton from "../../../components/UI/AddFriendButton";
 
 const Page: FC = () => {
-  return <main>
-    <h1 className="font-bold text-5xl mb-8">
-      <AddFriendButton />
-    </h1>
-  </main>
-}
+  return (
+    <main>
+      <h1 className="font-bold text-5xl mb-8">
+        <AddFriendButton />
+      </h1>
+    </main>
+  );
+};
 
 export default Page;
